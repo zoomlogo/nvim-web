9d6a5cee3bfa925083b36de84b6f6c39ba15e4f0
U-maanas\maanas <maanasyt@gmail.com>
2021-02-03 14:10:30 +0530

    Add special chars

--- rplugin/python3/nvim-web.py
+++ rplugin/python3/nvim-web.py
@@ -9,6 +9,36 @@ class Main(object):
     @neovim.command('GoogleSearch', nargs=1)
     def search(self, args):
         url = 'https://google.com/search?q='
+
+        # replace special chars
         url += args[0].replace(' ', '+')
+        url += args[0].replace('!', '%21')
+        url += args[0].replace('"', '%22')
+        url += args[0].replace('#', '%23')
+        url += args[0].replace('$', '%24')
+        url += args[0].replace('%', '%25')
+        url += args[0].replace('&', '%26')
+        url += args[0].replace('\'', '%27')
+        url += args[0].replace('*', '%2A')
+        url += args[0].replace('+', '%2B')
+        url += args[0].replace(',', '%2C')
+        url += args[0].replace('-', '%2D')
+        url += args[0].replace('.', '%2E')
+        url += args[0].replace('/', '%2F')
+        url += args[0].replace(':', '%3A')
+        url += args[0].replace(';', '%3B')
+        url += args[0].replace('<', '%3C')
+        url += args[0].replace('>', '%3E')
+        url += args[0].replace('=', '%3D')
+        url += args[0].replace('?', '%3F')
+        url += args[0].replace('@', '%40')
+        url += args[0].replace('|', '%7C')
+        url += args[0].replace('\\', '%5C')
+        url += args[0].replace('`', '%60')
+        url += args[0].replace('^', '%5E')
+        url += args[0].replace('~', '%7E')
+        url += args[0].replace('_', '%5F')
+
+        # open the page
         webbrowser.open_new(url)
 
