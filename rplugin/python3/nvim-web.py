4e5e7b5e299fb5477489d8106c62d48147fd981d
U-maanas\maanas <maanasyt@gmail.com>
2021-02-03 13:17:53 +0530

    WIP: Implemeting goole part 1

--- rplugin/python3/nvim-web.py
+++ rplugin/python3/nvim-web.py
@@ -1,11 +1,12 @@
 import neovim
+import webbrowser
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('DoItPython')
+    @neovim.command('GoogleSearch')
     def doItPython(self, args):
-        self.vim.command('echo "hello from DoItPython"')
+        self.vim.command(f'echo "hello from {args}"')
 
